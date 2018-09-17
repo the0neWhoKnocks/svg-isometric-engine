0bb8d0cba2c94f7d1a4a5dbf50b4cbfd05092853
Nox <the0neWhoKnocks@users.noreply.github.com>
2018-09-17 15:26:41 -0700

    TileBrowser loads tiles
    The TileBrowser can now load individual tiles or a whole folder of 
    tiles.

--- src/components/TilesBrowser/index.js
+++ src/components/TilesBrowser/index.js
@@ -1,16 +1,86 @@
 import React, { Component } from 'react';
+import Tile from './components/Tile';
+import styles from './styles';
 
 class TilesBrowser extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tiles: [],
+    };
+    this.handleTilesUpload = this.handleTilesUpload.bind(this);
+  }
+  
+  handleTilesUpload(ev) {
+    const files = ev.currentTarget.files;
+    const tiles = [];
+    
+    for(let i=0; i<files.length; i++){
+      const currFile = files[i];
+      // Only process image files.
+      if( !currFile.type.match('image.*') ) continue;
+
+      const reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = ((tile) => {
+        return (fileEv) => {
+          tiles.push({
+            image: fileEv.target.result,
+            name: escape(tile.name),
+          });
+          
+          // Once all the tiles have been loaded, update the view
+          if(tiles.length === files.length){
+            this.setState({
+              // TODO filter out duplicates, and maintain order
+              tiles: [
+                ...this.state.tiles,
+                ...tiles,
+              ],
+            });
+          }
+        };
+      })(currFile);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(currFile);
+    }
   }
 
   render() {
+    const { tiles } = this.state;
 
     return (
-      <div>Tiles Browser</div>
+      <div className={`${ styles.root }`}>
+        <nav className={`${ styles.nav }`}>
+          <input
+            type="file"
+            accept="image/*"
+            multiple="true"
+            onChange={ this.handleTilesUpload }
+          />
+          <input
+            type="file"
+            accept="image/*"
+            webkitdirectory="true"
+            mozdirectory="true"
+            onChange={ this.handleTilesUpload }
+          />
+        </nav>
+        <div className={`${ styles.tiles }`}>
+          {tiles.map(({ image, name }, ndx) => {
+            return (
+              <Tile
+                key={ name }
+                image={ image }
+                name={ name }
+              />
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
---
+++ src/components/TilesBrowser/styles.js
@@ -0,0 +1,21 @@
+import { css } from 'glamor';
+
+const styles = {
+  root: css({
+    paddingTop: '0.25em',
+  }),
+  
+  nav: css({
+    padding: '0.25em',
+    background: '#333',
+  }),
+  
+  tiles: css({
+    padding: '0.25em',
+    margin: '0.25em',
+    background: '#333',
+    overflowX: 'hidden',
+  }),
+};
+
+export default styles;
